330a3ee42e330776f05adf12a0e9290b603ef0f8
v-wang <victorwang920@gmail.com>
2014-12-23 20:07:54 -0500

    added image to users index. create projects model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223235841) do
+ActiveRecord::Schema.define(version: 20141224005133) do
 
   create_table "posts", force: true do |t|
     t.text     "message"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
