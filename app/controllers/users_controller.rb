f2dfc46e6367168760a3331d60ca1e015b7f149a
v-wang <victorwang920@gmail.com>
2014-12-23 19:21:22 -0500

    added post model and displayed registered users on users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_current_user, only: [:show]
   def index
+  	@users = User.all
   end
 
+  def show
+  end
  
-
-
+  private
+  def set_current_user
+  	@user = User.find(params[:id])
+  end
 end
